25570b543a22b7c4213a48b34b17b292e6ebc0f9
Alh004 <149379833+Alh004@users.noreply.github.com>
2024-08-27 14:09:24 +0200

    omstart

--- ClassRoomNet60/Student.cs
+++ ClassRoomNet60/Student.cs
@@ -1,16 +1,15 @@
 namespace ClassRoomNet60;
 
-public class student
+public class Student
 {
     public string Name { get; private set; }
     public int BirthMonth { get; private set; }
     public int BirthDay { get; private set; }
 
-    public student(string name, int birthMonth, int birthDay)
+    public Student(string name, int birthMonth, int birthDay)
     {
         Name = name;
         BirthMonth = birthMonth;
         BirthDay = birthDay;
-        
     }
 }
