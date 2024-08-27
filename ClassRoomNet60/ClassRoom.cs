11038cd9e6e06220067744ec208ab42ebf4d544d
Alh004 <149379833+Alh004@users.noreply.github.com>
2024-08-27 14:13:30 +0200

    ClassRoom.cs

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,15 @@
+namespace ClassRoomNet60;
+
+public class ClassRoom
+{
+    public string ClassName { get; private set; }
+    public List<Student>StudentList { get; private set; }
+    public DateTime Semesterstart { get; private set; }
+    
+    public ClassRoom ()
+    {
+        ClassName = "ClassRoomNet60";
+        StudentList = new List<Student>();
+        Semesterstart = new DateTime(2022, 9, 1);
+    }
+}
